6adaa063469f727fc488010b065fd098a3d9f2f9
matiasblobs <127749600+matiasblobs@users.noreply.github.com>
2023-08-09 12:12:46 -0300

    Update dbConnections.js

--- src/config/dbConnections.js
+++ src/config/dbConnections.js
@@ -3,10 +3,10 @@ const mysql=require('mysql');
 
 module.exports = ()=>{
     return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
+        host: '200.58.112.219',
+        user: 'o6000803_tpnode',
         insecureAuth : true,
-        password: '',
-        database: 'libreria03'
+        password: 'seweGUbi27',
+        database: 'o6000803_tpnode'
     });
 } 
