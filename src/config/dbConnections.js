ec9b7b44652e206dc9e3999718bd2f5324317691
matiasblobs <127749600+matiasblobs@users.noreply.github.com>
2023-08-09 19:17:50 -0300

    Update dbConnections.js

--- src/config/dbConnections.js
+++ src/config/dbConnections.js
@@ -3,11 +3,10 @@ const mysql=require('mysql');
 
 module.exports = ()=>{
     return mysql.createConnection({
-        host: 'sql.freedb.tech',
-        user: 'freedb_matiasblobs',
+        host: '149.100.155.204',
+        user: 'u236440595_nodetest',
         insecureAuth : true,
-        password: 'An*Hm$?uVY4z92U',
-        database: 'freedb_testeonode',
-        port: 3306
+        password: 'Charlotte@4411',
+        database: 'u236440595_nodetest'
     });
 } 
