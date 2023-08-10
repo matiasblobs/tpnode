00c367612ac7c963076d5502e2aa624bfaea1798
matiasblobs <127749600+matiasblobs@users.noreply.github.com>
2023-08-10 11:19:05 -0300

    Update server.js

--- src/config/server.js
+++ src/config/server.js
@@ -2,15 +2,16 @@
 const express = require ('express');
 const path = require ('path');
 const bodyParser = require('body-parser');
-
 const app = express();
-
 
 
 
 // Settings
 //app.set('port', process.env.PORT || 3000); // le decimos que elija un puerto o que tome el 3000
-app.set('port', process.env.PORT ?? 8080); // linea para flO.com
+
+// IMPORTANTE: se cambió la linea de conexión al puerto, para que funcione con la página fl0.com, que es dónde hice el DEPLOY
+app.set('port', process.env.PORT ?? 8080); 
+
 app.set('view engine', 'ejs'); // definimos cual es nuestro motor de plantillas
 app.set('views', path.join(__dirname, '../app/views')); // arma las rutas, concatenando el directorio raiz con el detalle que les pase
 
