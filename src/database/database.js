10caeaed93f45046ec32a0bfd757a518f3a3b5c8
matiasblobs <127749600+matiasblobs@users.noreply.github.com>
2023-11-16 17:32:47 -0300

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@
 import Sequelize from "sequelize";
 
 // nombre de base de datos, usuario, contraseña
-export const sequelize = new Sequelize("tpfinal", "postgres", "1234", {
-    host: "localhost",
+export const sequelize = new Sequelize("tpfinal", "fl0user", "KsN4tLl2CoDj", {
+    host: "ep-sparkling-leaf-49395978.ap-southeast-1.aws.neon.fl0.io",
     dialect: "postgres",
 });  
