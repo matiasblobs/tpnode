d8dcd406188fbaf12203070aecc51b05de37d371
matiasblobs <127749600+matiasblobs@users.noreply.github.com>
2023-11-16 17:51:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ async function main() {
         await sequelize.sync({force:false});
         console.log('Base de datos conectada');
         // Podemos cambiar el puerto
-        app.listen(8080);
+        app.listen(10.2.35.234:8080);
         console.log("Servidor conectado en el puerto 8080");
 
 } catch (error) {
